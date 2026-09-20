43bf2d78de6ce2dd8bd2fc7e28ae4d2dd3b682a9
Saifur Rahman Saif <saif1019262@gmail.com>
2026-09-20 20:16:13 +0600

    add tto folders

---
+++ Basic_C_Program/calculator4_8.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main (){
+    int a,b;
+    char ch;
+    printf("Enter here : ");
+    scanf("%d %c %d",&a,&ch,&b); //intput number a then +,-,*,/,% and then number b
+    if(ch== '+')printf("%d",a+b);
+    else if(ch=='-')printf("%d",a-b);//exp : 5 - 2 =3
+    else if(ch=='*')printf("%d",a*b);
+    else if(ch=='/')printf("%d",a/b);
+    else if(ch=='%')printf("%d",a%b);//exp : 5 % 2 =1
+    else printf("please enter a valid arethmetic operator");
+   return 0;
+}
